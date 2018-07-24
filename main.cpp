098d65ede4731fe98a56ac13a7d7f87ebba2c448
ggawryal <ggawryal@hotmail.com>
2018-07-24 23:24:26 +0200

    new add checker option, draw checkers, fixed and linked setImageSize and setCellSize

--- main.cpp
+++ main.cpp
@@ -44,12 +44,18 @@ public:
 
 enum Checkers {empty = 0, white_pawn = 1, white_queen = 2, black_pawn = 3, black_queen = 4};
 
+struct CheckerOnGrid{
+    int x,y;
+    sf::Sprite sprite;
+};
+
 class CheckBoardDrawer : public sf::Drawable{
 public:
     vector<vector<sf::Sprite> > checkboard;
+    vector<CheckerOnGrid > checkers;
     sf::Vector2i position = sf::Vector2i(0,0);
     sf::Vector2f cellSize = sf::Vector2f(0,0);
-   // vector<sf::Sprite > checkers;
+
     void resizeBoard(int x,int y){
         checkboard.clear();
         checkboard.resize(x);
@@ -75,40 +81,66 @@ public:
                 checkboard[i][j].setPosition(sf::Vector2f(position.x+i*cellSize.x,position.y+j*cellSize.y));
             }
         }
+        for(auto &a:checkers)
+            a.sprite.setPosition(sf::Vector2f(position.x+a.x*cellSize.x,position.y+a.y*cellSize.y));
+
     }
     void setCellSize(int x,int y){
+
+        sf::Vector2u standardSize = checkboard[0][0].getTexture()->getSize();
+        cellSize = sf::Vector2f(x,y);
+
         for(auto& column:checkboard)
             for(auto &sprite : column)
-                sprite.setScale(x/(float)cellSize.x, y/(float)cellSize.y);
-        cellSize = sf::Vector2f(x,y);
+                sprite.setScale(cellSize.x/(float)standardSize.x, cellSize.y/(float)standardSize.y);
+        for(auto &checker :checkers)
+            checker.sprite.setScale(cellSize.x/(float)standardSize.x, cellSize.y/(float)standardSize.y);
+
+
         fixSprites();
     }
     void setImageSize(int x,int y){
         assert(checkboard.size() > 0);
         assert(checkboard[0].size() > 0);
-        sf::Vector2u standardSize = checkboard[0][0].getTexture()->getSize();
 
         cellSize = sf::Vector2f(x/(float)(checkboard.size()), y/(float)(checkboard[0].size()));
-
-        for(auto& column:checkboard)
+        setCellSize(cellSize.x,cellSize.y);
+        /*for(auto& column:checkboard)
             for(auto &sprite : column)
                 sprite.setScale(cellSize.x/(float)standardSize.x, cellSize.y/(float)standardSize.y);
-        fixSprites();
-    }
 
+        for(auto &checker :checkers)
+            checker.sprite.setScale(cellSize.x/(float)standardSize.x, cellSize.y/(float)standardSize.y);
+        fixSprites();*/
+    }
+    void addCheckerImage(int x,int y,Checkers type){
+        checkers.push_back(CheckerOnGrid());
+        CheckerOnGrid& newChecker = checkers.back();
+        newChecker.x = x;
+        newChecker.y = y;
+        assert(type != Checkers::empty);
+         if(type == Checkers::black_pawn)
+            newChecker.sprite.setTexture(TextureManager::instance().get("blackPawn 1"));
+        if(type == Checkers::black_queen)
+            newChecker.sprite.setTexture(TextureManager::instance().get("blackQueen 1"));
+        if(type == Checkers::white_pawn)
+            newChecker.sprite.setTexture(TextureManager::instance().get("whitePawn 1"));
+        if(type == Checkers::white_queen)
+            newChecker.sprite.setTexture(TextureManager::instance().get("whiteQueen 1"));
+    }
     void updateSize(vector<vector<Checkers> >& board){
         if(board.size() != checkboard.size() || (board.size() > 0 && board[0].size() != checkboard[0].size()))
             resizeBoard(board.size(),board[0].size());
 
     }
-
     virtual void draw(sf::RenderTarget& target,sf::RenderStates states) const override{
-        for(int i=0;i<checkboard.size();i++){
-            for(int j=0;j<checkboard[i].size();j++){
+        for(int i=0;i<checkboard.size();i++)
+            for(int j=0;j<checkboard[i].size();j++)
                 target.draw(checkboard[i][j],states);
-                //target.draw(checkers[i][j],states);
-            }
-        }
+
+        for(auto &a:checkers)
+            target.draw(a.sprite,states);
+
     }
 };
 
@@ -131,6 +163,24 @@ public:
         drawer.fixSprites();
         drawer.updateSize(board);
     }
+
+    void addChecker(int x,int y,Checkers type){
+        assert(x < board.size());
+        assert(board.size() > 0);
+        assert(y < board[0].size());
+
+        if(board[x][y] != Checkers::empty){
+            cout<<"Warning: cell "<<x<<" "<<y<<" not empty, already "<<(int)board[x][y]<<" on it"<<endl;
+            for(int i=0;i<drawer.checkers.size();i++){
+                if(drawer.checkers[i].x == x && drawer.checkers[i].y == y){
+                    drawer.checkers.erase(drawer.checkers.begin()+i);
+                    break;
+                }
+            }
+        }
+        board[x][y] = type;
+        drawer.addCheckerImage(x,y,type);
+    }
 };
 
 
@@ -146,9 +196,12 @@ int main(){
     textureManager.loadAll();
     Checkboard checkboard;
     checkboard.setSize(8,8);
-    checkboard.drawer.setCellSize(80,80);
-    //checkboard.drawer.setImageSize(700,700);
 
+    checkboard.addChecker(2,2,Checkers::black_pawn);
+    checkboard.addChecker(3,3,Checkers::black_pawn);
+    checkboard.addChecker(4,4,Checkers::white_queen);
+
+    checkboard.drawer.setImageSize(800,800);
     while (window.isOpen()){
         sf::Event event;
         while (window.pollEvent(event)){
